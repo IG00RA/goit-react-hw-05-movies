1a42a051174222d2c3049c14570733a2327db41a
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-05-11 09:39:32 +0300

    update src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,3 @@
-import { BrowserRouter } from 'react-router-dom';
-
 export const App = () => {
-  return (
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      <App />
-    </BrowserRouter>
-  );
+  return <div>123</div>;
 };
